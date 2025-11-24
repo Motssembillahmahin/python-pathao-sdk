6ac32082cf801e7a946f4bbb50be41889a2564c3
Motssembillahmahin <asmahin8@gmail.com>
2025-11-25 01:30:07 +0600

    refactor: remove unnecessary fastapi configuration

--- src/main.py
+++ src/main.py
@@ -3,9 +3,6 @@ from contextlib import asynccontextmanager
 from typing import TYPE_CHECKING
 
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
-
-from src.config import settings, app_configs
 
 if TYPE_CHECKING:
     pass
@@ -19,18 +16,8 @@ async def lifespan(_application: FastAPI) -> AsyncGenerator:
 
 
 app = FastAPI(
-    **app_configs,
     lifespan=lifespan,
     swagger_ui_parameters={"persistAuthorization": True},
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,
-    allow_origin_regex=settings.CORS_ORIGINS_REGEX,
-    allow_credentials=True,
-    allow_methods=("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"),
-    allow_headers=settings.CORS_HEADERS,
 )
 
 
