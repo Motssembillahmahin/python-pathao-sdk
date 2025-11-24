08639000e34dce0f6f929b6952448848c5a7c5c2
Motssembillahmahin <asmahin8@gmail.com>
2025-11-25 02:03:31 +0600

    chore: configure base class for API resources

---
+++ src/resources/__init__.py
@@ -0,0 +1,18 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from src.client import PathaoClient
+
+
+class BaseResource:
+    """Base class for API resources"""
+
+    def __init__(self, client: "PathaoClient"):
+        self._client = client
+        self._http = client.http_client
+
+    async def _request(self, method: str, endpoint: str, **kwargs):
+        """Make authenticated request"""
+        response = await self._http.request(method, endpoint, **kwargs)
+        response.raise_for_status()
+        return response.json()
